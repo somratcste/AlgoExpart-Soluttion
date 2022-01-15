bcbd702fa9baf0fd946d0036e3052439fe193e20
somratcste <somratcste@gmail.com>
2022-01-15 21:24:30 +0100

    task: recursive solution

--- src/Recursions/Powerset.java
+++ src/Recursions/Powerset.java
@@ -17,4 +17,27 @@ public class Powerset {
         }
         return subsets;
     }
+
+    // recursive solution
+    // O(n * 2 power n) time | O(n * 2 power n) space
+    /*public static List<List<Integer>> powerset(List<Integer> array) {
+        return powerset(array, array.size() - 1);
+    }
+
+    public static List<List<Integer>> powerset(List<Integer> array, int idx) {
+        if (idx < 0) {
+            List<List<Integer>> emptySet = new ArrayList<List<Integer>>();
+            emptySet.add(new ArrayList<Integer>());
+            return emptySet;
+        }
+        int ele = array.get(idx);
+        List<List<Integer>> subsets = powerset(array, idx-1);
+        int length = subsets.size();
+        for (int i=0; i<length; i++) {
+            List<Integer> currentSubset = new ArrayList<Integer>(subsets.get(i));
+            currentSubset.add(ele);
+            subsets.add(currentSubset);
+        }
+        return subsets;
+    }*/
 }
