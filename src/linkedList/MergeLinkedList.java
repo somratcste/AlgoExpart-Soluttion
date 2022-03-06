b95ca7d67d2012c5f8bc1d5e34c0a09b3c8ad3bb
somratcste <somratcste@gmail.com>
2022-03-06 10:25:28 +0100

    task: merge linked list

---
+++ src/linkedList/MergeLinkedList.java
@@ -0,0 +1,34 @@
+package linkedList;
+
+public class MergeLinkedList {
+    // This is an input class. Do not edit.
+    public static class LinkedList {
+        int value;
+        LinkedList next;
+
+        LinkedList(int value) {
+            this.value = value;
+            this.next = null;
+        }
+    }
+
+    // O(n+m) time | O(1) space
+    public static LinkedList mergeLinkedLists(LinkedList headOne, LinkedList headTwo) {
+        LinkedList p1 = headOne;
+        LinkedList p1Prev = null;
+        LinkedList p2 = headTwo;
+        while (p1 != null && p2 != null) {
+            if(p1.value < p2.value) {
+                p1Prev = p1;
+                p1 = p1.next;
+            } else {
+                if (p1Prev != null) p1Prev.next = p2;
+                p1Prev = p2;
+                p2 = p2.next;
+                p1Prev.next = p1;
+            }
+        }
+        if(p1 == null) p1Prev.next = p2;
+        return headOne.value < headTwo.value ? headOne : headTwo;
+    }
+}
